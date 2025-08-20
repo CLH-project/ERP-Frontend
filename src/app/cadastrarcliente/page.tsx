1ae9659d862171cdee069dd96e5d1323675a6456
LuizHenri16 <lbastos722@gmail.com>
2025-08-19 22:20:43 -0300

    feature: adicionado erros para cada tipo de erro em validação e mensagem de sucesso ao cadastrar cliente, botão de voltar na tela de cliente

--- src/app/cadastrarcliente/page.tsx
+++ src/app/cadastrarcliente/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 
-import {ClienteCadastroForm, Sidebar } from "@/components";
+import {Button, ClienteCadastroForm, Sidebar } from "@/components";
+import { useRouter } from "next/navigation";
 
 export default function cadastrarCliente() {
+
+    const router = useRouter();
+
     return (
         <div className="w-[90%] h-screen flex flex-col items-center mx-auto">
             <div className="w-full flex justify-between items-center p-2">
@@ -11,9 +15,16 @@ export default function cadastrarCliente() {
             </div>
 
             <div>
-                <div className="w-full"><h1 className="text-2xl font-bold mb-10 ml-5">Cliente</h1></div>
+                <div className="w-full">
+                    <h1 className="text-2xl font-bold mb-10 ml-5">Cliente</h1>
+                </div>
+                
                 <div className="flex flex-col items-center justify-center px-5 py-6 bg-gray-200 rounded-2xl">
                     <ClienteCadastroForm />
+                </div>
+                    
+                <div className="w-full flex justify-center mt-5">
+                    <Button functionName="Voltar" type="button"  onClick={() => router.back()}/>
                 </div>
             </div>
         </div>
--- src/components/clienteCadastroForm/clienteCadastroForm.tsx
+++ src/components/clienteCadastroForm/clienteCadastroForm.tsx
@@ -1,60 +1,79 @@
+'use client'
+
+import React, { useState } from "react";
 import { Formik, Form } from "formik";
 import { TextField, Button, ErrorMessagePop, MaskedTextField } from "@/components";
 import * as Yup from "yup";
+import axios from "axios";
 
 export const ClienteCadastroForm: React.FC = () => {
-    
-    return (
-        <Formik 
-        initialValues={{ nome: "", cpf: "", telefone: "" }}
-        validationSchema={Yup.object({
-            nome: Yup.string().required(),
-            cpf: Yup.string().required(),
-            telefone: Yup.string().required(), 
-        })}        
 
-        onSubmit={
+    const [SucessMessage, setSucessMessage] = useState("")
+
+    return (
+        <Formik
+            initialValues={{ nome: "", cpf: "", telefone: "" }}
+            validationSchema={Yup.object({
+                nome: Yup.string().required(),
+                cpf: Yup.string().required(),
+                telefone: Yup.string().required()
+            })}
+
+             onSubmit={
             async (values, { setSubmitting, setErrors }) => {
-                try {
-                    const response = await fetch("localhost:8080/clientes", {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body: JSON.stringify(values),
-                    });     
-                }
-                catch (err: any) {
-                    alert("Erro ao cadastrar cliente. Por favor, tente novamente mais tarde.");
-                } finally {
-                    setSubmitting(false);
+                    
+                    
+                    axios.post("http://localhost:8080/clientes", values)
+                        .then((response) => {
+                            if (response.status === 201) {
+                                setSucessMessage(response.data.message);
+                            }
+                            setSubmitting(false);
+                        })
+                        .catch((error) => {
+
+                            if (error.response && error.response.status === 400 && error.response.data && error.response.data.messages) {
+                                const message = error.response.data.messages;
+                                const fieldErrors = {nome: "", cpf: "", telefone: "" };
+
+                                if(message.nome) {fieldErrors.nome = message.nome;}
+                                if(message.cpf) {fieldErrors.cpf = message.cpf;}
+                                if(message.telefone) {fieldErrors.telefone = message.telefone;}
+
+                                setErrors(fieldErrors);
+                                setSucessMessage("")
+                            }
+                            setSubmitting(false);
+                        });
                 }
             }
-        }
         >
-            {({ isSubmitting }) => (    
-                <Form className="flex flex-col gap-5"> 
+            {({ isSubmitting }) => (
+                <Form className="flex flex-col gap-5">
                     <div >
                         <label htmlFor="nome">Nome do Cliente</label>
                         <TextField name="nome" type="text" placeholder="Digite o nome do cliente" />
                         <ErrorMessagePop name="nome" component="div" />
                     </div>
-                    
+
                     <div >
                         <label htmlFor="cpf">CPF do Cliente</label>
-                        <MaskedTextField name="cpf" mask="XXX.XXX.XXX-XX" placeholder="Digite o CPF do cliente"/>
+                        <MaskedTextField name="cpf" mask="XXX.XXX.XXX-XX" placeholder="Digite o CPF do cliente" />
                         <ErrorMessagePop name="cpf" component="div" />
                     </div>
 
                     <div>
                         <label htmlFor="nome">Telefone do Cliente</label>
-                        <MaskedTextField name="telefone" mask="(XX) XXXXX-XXXX" placeholder="Digite o telefone do cliente"/>
-                        <ErrorMessagePop name="telefone" component="div"/>
+                        <MaskedTextField name="telefone" mask="(XX) XXXXX-XXXX" placeholder="Digite o telefone do cliente" />
+                        <ErrorMessagePop name="telefone" component="div" />
                     </div>
-                    <Button functionName="Adicionar Cliente" type="submit" disabled={isSubmitting}/>
+                    <Button functionName="Adicionar Cliente" type="submit" disabled={isSubmitting} />
+                    {SucessMessage && <div className="text-center p-4 rounded-2xl bg-green-200 text-green-800">{SucessMessage}</div>}
                 </Form>
+                
             )}
 
         </Formik>
+        
     )
 }
